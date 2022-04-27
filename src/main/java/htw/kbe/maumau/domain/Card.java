cfef30f37a3ef802cbbda473f59137c08a736cb4
RiFrost <ri.frost@web.de>
2022-04-27 14:30:59 +0200

    rf add Card and Deck class with first functions

--- src/main/java/htw/kbe/maumau/domain/Card.java
+++ src/main/java/htw/kbe/maumau/domain/Card.java
@@ -1,8 +1,25 @@
 package htw.kbe.maumau.domain;
 
-public class Card {
+public class Card implements ICard {
 
     private Suit suit;
     private Value value;
-  
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s of %s", this.value, this.suit);
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Value getValue() {
+        return value;
+    }
 }
--- src/main/java/htw/kbe/maumau/domain/Deck.java
+++ src/main/java/htw/kbe/maumau/domain/Deck.java
@@ -1,10 +1,62 @@
 package htw.kbe.maumau.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Deck {
+public class Deck implements IDeck {
 
     private List<Card> cards;
     private long totalAmount;
 
+    private Deck(List<Card> cards) {
+        this.cards = cards;
+        this.totalAmount = cards.stream().count();
+    }
+
+    public static Deck build() {
+        List<Card> cards = new ArrayList<Card>();
+
+        for(Suit suit : Suit.values()) {
+            for(Value value : Value.values()) {
+                cards.add(new Card(suit, value));
+            }
+        }
+
+        return new Deck(cards);
+    }
+
+    @Override
+    public void shuffle() {
+        for(int i = 0; i < this.cards.size(); i++) {
+            int randomIndex = (int) (Math.random() * this.cards.size());
+            Card newCard = this.cards.get(randomIndex);
+            this.cards.set(randomIndex, this.cards.get(i));
+            this.cards.set(i, newCard);
+        }
+    }
+
+    @Override
+    public List<Card> drawCards(int amount) {
+        List<Card> returnList = new ArrayList<Card>();
+
+        while(amount > 0) {
+            try {
+                returnList.add(cards.remove(0));
+            } catch(Exception e) {
+                System.out.println("error deck is empty");
+            }
+
+            amount--;
+        }
+
+        return returnList;
+    }
+
+    public List<Card> getCards() {
+        return this.cards;
+    }
+
+    public long getTotalAmount() {
+        return this.cards.stream().count();
+    }
 }

3f6c6f01571e28ed12626a1a93ec32c2acfb472e
Jasmin <jasmin.quast@student.htw-berlin.de>
2022-05-21 23:47:41 +0200

    JQ - java doc for PlayerService and new exception

--- player/src/main/java/htw/kbe/maumau/player/service/PlayerService.java
+++ player/src/main/java/htw/kbe/maumau/player/service/PlayerService.java
@@ -2,15 +2,40 @@ package htw.kbe.maumau.player.service;
 
 import htw.kbe.maumau.card.domain.Card;
 import htw.kbe.maumau.player.domain.Player;
+import htw.kbe.maumau.player.exceptions.IllegalPlayerSizeException;
+
 import java.util.List;
 
 public interface PlayerService {
 
-    Player createNewPlayer(String id, String name);
+    /**
+     * initialise player
+     * @param id - Player who wants to join the game
+     * @param name - Name of the player
+     * @return player who is ready to play
+     * @throws IllegalPlayerSizeException when the team is already full (size of max. 4 Players)
+     */
+    Player createNewPlayer(String id, String name) throws IllegalPlayerSizeException;
 
+    /**
+     * cards that are drawn by a player
+     * @param player active player
+     * @param cards cards that are drawn
+     * @return list of cards
+     */
     Player drawCards(Player player, List<Card> cards);
 
+    /**
+     * card that is played by a player and will be discarded
+     * @param player active player
+     * @return card that is played
+     */
     Card playCard(Player player);
 
+    /**
+     * a message by a player, who only has one card left in hand
+     * @param player active player
+     * @return message "Maumau"
+     */
     Player sayMauMau(Player player);
 }

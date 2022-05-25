8ef144bf2c6587f68f4340324796345ae90d2360
RiFrost <ri.frost@web.de>
2022-05-25 15:07:43 +0200

    rf add first gameservice tests

---
+++ game/src/test/java/htw/kbe/maumau/game/fixtures/GameFixture.java
@@ -0,0 +1,68 @@
+package htw.kbe.maumau.game.fixtures;
+
+import htw.kbe.maumau.card.domain.Card;
+import htw.kbe.maumau.card.domain.Label;
+import htw.kbe.maumau.card.domain.Suit;
+import htw.kbe.maumau.deck.domain.Deck;
+import htw.kbe.maumau.game.domain.Game;
+import htw.kbe.maumau.player.domain.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class GameFixture {
+
+    private static List<Player> players = new LinkedList<Player>();
+
+
+    public static List<Player> players() {
+        players.add(new Player(1, "Phil"));
+        players.add(new Player(2, "Maria"));
+        players.add(new Player(3, "Jasmin"));
+        players.add(new Player(4, "Richard"));
+
+        return players;
+    }
+
+    public static Deck deck() {
+        Deck deck = new Deck();
+        deck.setDrawPile(cards());
+        return deck;
+    }
+
+    public static Game game() {
+        return new Game(players(), deck());
+    }
+
+    public static List<Suit> suits = Arrays.asList(
+            Suit.CLUBS,
+            Suit.DIAMONDS,
+            Suit.HEARTS,
+            Suit.SPADES
+    );
+
+    public static List<Label> labels = Arrays.asList(
+            Label.ASS,
+            Label.KING,
+            Label.QUEEN,
+            Label.JACK,
+            Label.TEN,
+            Label.NINE,
+            Label.EIGHT,
+            Label.SEVEN
+    );
+
+    public static List<Card> cards() {
+        List<Card> cards = new ArrayList<>();
+        for(Suit suit : suits) {
+            for(Label label : labels) {
+                cards.add(new Card(suit, label));
+            }
+        }
+
+        return cards;
+    }
+
+}
--- game/src/test/java/htw/kbe/maumau/game/service/GameServiceTest.java
+++ game/src/test/java/htw/kbe/maumau/game/service/GameServiceTest.java
@@ -1,12 +1,92 @@
 package htw.kbe.maumau.game.service;
 
+import htw.kbe.maumau.card.service.CardService;
+import htw.kbe.maumau.deck.exceptions.IllegalDeckSizeException;
+import htw.kbe.maumau.deck.service.DeckService;
+import htw.kbe.maumau.game.domain.Game;
+import htw.kbe.maumau.game.fixtures.GameFixture;
+import htw.kbe.maumau.player.domain.Player;
+import htw.kbe.maumau.player.service.PlayerService;
+import htw.kbe.maumau.rule.domain.Rule;
+import htw.kbe.maumau.rule.service.RulesService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class GameServiceTest {
 
-    @Test
-    public void testMethod() {
-        System.out.println("Es funktioniert!");
+    @InjectMocks
+    private GameService gameService;
+
+    @Mock
+    private DeckService deckService;
+
+
+    private List<Player> players = GameFixture.players();
+
+    @BeforeEach
+    public void setUp() throws IllegalDeckSizeException {
+        gameService = new GameServiceImpl();
+        deckService = mock(DeckService.class);
+        when(deckService.createDeck(anyList())).thenReturn(GameFixture.deck());
     }
 
+    @Test
+    @DisplayName("should return a new instance of game with Cards and the Playerlist")
+    public void startNewGame() throws IllegalDeckSizeException {
+        Game game = gameService.startNewGame(players);
+
+        assertEquals(players, game.getPlayers());
+        assertEquals(players.get(0), game.getActivePlayer());
+        assertNotEquals(null, game.getCardDeck().getTopCard());
+        assertEquals(31, game.getCardDeck().getDrawPile().size());
+        assertTrue(game.getClockWise());
+    }
+
+    @Test
+    @DisplayName("should return the next player in clockwise direction")
+    public void nextPlayerClockWise() throws IllegalDeckSizeException {
+        Game game = gameService.startNewGame(players);
+        Player activePlayer = players.get(0);
+        Player nextActivePlayer = players.get(1);
+
+        assertEquals(activePlayer, game.getActivePlayer());
+
+        gameService.nextPlayer(game);
+
+        assertEquals(nextActivePlayer, game.getActivePlayer());
+    }
+
+    @Test
+    @DisplayName("should return the next player in counter clockwise direction")
+    public void nextPlayerCounterClockWise() throws IllegalDeckSizeException {
+        Game game = gameService.startNewGame(players);
+        Player activePlayer = players.get(0);
+        Player nextActivePlayer = players.get(players.size() - 1);
+
+        assertEquals(activePlayer, game.getActivePlayer());
+        game.switchDirection();
+        gameService.nextPlayer(game);
+
+        assertEquals(nextActivePlayer, game.getActivePlayer());
+    }
+
+    @Test
+    @DisplayName("should return true if the Player does have the card in hand")
+    public void hasPlayerHandCard() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("should return false if the Player does not have the card in hand")
+    public void hasNotPlayerHandCard() {
+        assertTrue(true);
+    }
 }

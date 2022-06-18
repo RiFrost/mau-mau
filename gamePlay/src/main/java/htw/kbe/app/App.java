b31f4a0c74b8a7681c1eaf682eef3ea593a389f1
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-06-18 13:09:26 +0200

    MH: remove logger from main class

--- gamePlay/src/main/java/htw/kbe/app/App.java
+++ gamePlay/src/main/java/htw/kbe/app/App.java
@@ -1,22 +1,15 @@
 package htw.kbe.app;
 
 import htw.kbe.config.AppConfig;
-import htw.kbe.maumau.game.service.GameServiceImpl;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
-
-    private static Logger logger = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
             final AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
             var app = applicationContext.getBean(AppConfig.class);
 
-            logger.error("SAMPLE ERROR MESSAGE FOR APP");
-
-            app.appController().playGame();
+            app.appController().play();
 
             applicationContext.close();
         }

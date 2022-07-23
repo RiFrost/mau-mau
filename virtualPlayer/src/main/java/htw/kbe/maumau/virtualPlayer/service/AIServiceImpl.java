b04db9f7d3ed1ba963658b98a66454b2219c4fd6
RiFrost <ri.frost@web.de>
2022-07-23 11:58:51 +0200

    remove cardService from aiService

--- virtualPlayer/src/main/java/htw/kbe/maumau/virtualPlayer/service/AIServiceImpl.java
+++ virtualPlayer/src/main/java/htw/kbe/maumau/virtualPlayer/service/AIServiceImpl.java
@@ -1,7 +1,6 @@
 package htw.kbe.maumau.virtualPlayer.service;
 
 import htw.kbe.maumau.card.export.Card;
-import htw.kbe.maumau.card.export.CardService;
 import htw.kbe.maumau.card.export.Label;
 import htw.kbe.maumau.card.export.Suit;
 import htw.kbe.maumau.player.export.Player;
@@ -11,20 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class AIServiceImpl implements AIService {
-
-    @Autowired
-    private CardService cardService;
 
     @Autowired
     private RulesService rulesService;
 
     @Override
     public Card getPlayedCard(Player AI, Card topCard, Suit suitWish, int drawCounter) {
-        for (Card card: AI.getHandCards()) {
+        for (Card card : AI.getHandCards()) {
             if (rulesService.matchLabelOrSuit(card, topCard) || rulesService.isSuitWishValid(suitWish, card.getSuit())) {
                 if (topCard.getLabel().equals(Label.SEVEN) && drawCounter >= rulesService.getDefaultNumberOfDrawnCards()) {
                     if (rulesService.canPlaySeven(card.getLabel(), topCard.getLabel(), drawCounter)) {
@@ -42,13 +37,13 @@ public class AIServiceImpl implements AIService {
 
     @Override
     public boolean sayMau(Player AI) {
-        return AI.getHandCards().size() == 1;
+        return AI.getHandCards().size() == 2;
     }
 
     @Override
     public Suit getSuitWish(Player AI) {
-        List<Suit> validSuits = AI.getHandCards().stream().map(Card::getSuit).filter(item -> cardService.getSuits().contains(item)).collect(Collectors.toList());
-        return validSuits.get((int) (Math.random()*validSuits.size()));
+        List<Suit> validSuits = AI.getHandCards().stream().map(Card::getSuit).distinct().toList();
+        return validSuits.get((int) (Math.random() * validSuits.size()));
     }
 
 }
